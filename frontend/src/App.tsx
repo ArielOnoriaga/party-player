3df399ba0e2c62e0c820a71d379de14dae359385
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-11-20 16:07:12 -0300

    feat: add material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,11 @@
-const App = () => <h1> This is a test component </h1>;
+import Button from '@mui/material/Button';
+
+const App = () => (
+    <div>
+        <Button
+            variant="contained"> This is a material Button </Button>
+        <h1> This is a test component </h1>
+    </div>
+);
 
 export default App;
