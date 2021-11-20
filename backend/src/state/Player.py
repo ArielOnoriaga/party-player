4d4d858cb50e9f47692b9c4dadc1cb1380b9aa86
federf <fede051993@gmail.com>
2021-11-19 21:33:56 -0300

    Player: fix play a song

--- backend/src/state/Player.py
+++ backend/src/state/Player.py
@@ -24,19 +24,12 @@ class Player:
     def play(self, uri: str):
         devices = Devices().get()
         firstDevice = devices[0]['id']
-        #curl -X "PUT" "https://api.spotify.com/v1/me/player/play?device_id=a821c70236ad6919fea915982b819f907330ebfb" --data "{\"context_uri\":\"spotify:album:1bTgKomQYSkKYPD9UI9W4b\",\"offset\":{\"position\":0},\"position_ms\":0}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQAPK0dpF26tbOxUfe_x_3vv8uNMlcwWtxjEfI2cUzwmMaA_QcVQx_sNLYsabuRFSUtAuuPiyA_X2Xj26YDrtASCbqXHWqR9iY147CVXiaUnXtvhL11k9A4VuomXfNquP9uJ4V76k4jkRhrCT_KjfryU6I9FjVC1OL0CMTMwWbShLNgkgPKaNIDhf1tHk7k"
 
         playUrl = f"https://api.spotify.com/v1/me/player/play?device_id={firstDevice}"
-        uri = 'spotify:album:1bTgKomQYSkKYPD9UI9W4b';#imagine dragons enemy
+        requestData = {"context_uri": uri,"offset": {"position": 0},"position_ms": 0}
         response = requests.put(
             playUrl,
             headers = self.headers,
-            data = {
-                "context_uri": uri,
-                "offset": {
-                    "position": 0
-                },
-                "position_ms": 0
-            }
+            data = json.dumps(requestData)
         )
-        return response.json()
+        return True
