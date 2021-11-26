2bb055e8b6c237df7e8ec1ae43fb8477f81242bb
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-11-26 19:04:47 -0300

    back-32 | feat: add socketio

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_restful import Api, reqparse
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
-from src.routes.Autorize import Autorize
+from src.routes.Autorize import autorization
+from src.routes.Player import player
+
 from src.routes.SearchRoutes import SearchSomething
-
 from src.routes.TokenRoutes import TokenGet, TokenRead, TokenRefresh
-from src.routes.PlayerRoutes import PlaySomething, GetDevices, PlayerState, Pause, Resume, SetVolume
-
 app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+app.config.update(
+    host = '0.0.0.0',
+    port = 8080,
+    debug=True,
+)
 
 api = Api(app)
 
@@ -26,24 +34,22 @@ CORS(
     upport_credentials=True
 )
 
-api.add_resource(Autorize, '/autorize/')
+app.register_blueprint(autorization)
+app.register_blueprint(player, url_prefix='/player')
+
 api.add_resource(SearchSomething, '/search/')
 api.add_resource(TokenGet, '/callback/')
 api.add_resource(TokenRead, '/token/read/')
 api.add_resource(TokenRefresh, '/token/refresh/')
 
-api.add_resource(PlayerState, '/player/state/')
-api.add_resource(PlaySomething, '/player/play/')
-api.add_resource(Pause, '/player/pause')
-api.add_resource(Resume, '/player/resume')
 api.add_resource(GetDevices, '/player/devices')
 api.add_resource(SetVolume, '/player/volume')
 
 parser = reqparse.RequestParser()
 
 if __name__ == "__main__":
-    app.run(
-        debug=True,
+    socketio.run(
+        app,
         port=8080,
         host='0.0.0.0'
     )
