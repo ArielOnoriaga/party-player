14f4c3b07472609a9071790f58e609fd9e88561d
federf <fede051993@gmail.com>
2021-12-11 23:44:48 -0300

    Toke routes: fix and improve code quality

--- backend/src/routes/TokenRoutes.py
+++ backend/src/routes/TokenRoutes.py
@@ -1,16 +1,21 @@
-from flask_restful import Resource
 from flask import request
+
+from flask_restful import Resource
+
 from src.TokenRetriever import TokenRetriever
+
 
 class TokenGet(Resource):
     def get(self):
         code = request.args.get('code')
         return TokenRetriever().get(code)
 
+
 class TokenRead(Resource):
     def get(self):
-        return TokenRetriever().read();
+        return TokenRetriever().read()
+
 
 class TokenRefresh(Resource):
     def get(self):
-        return TokenRetriever().refresh();
+        return TokenRetriever().refresh()
