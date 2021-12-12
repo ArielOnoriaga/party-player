f95863b0d11a46641c9ae1909c8b90e224e1e954
federf <fede051993@gmail.com>
2021-12-11 22:52:28 -0300

    [41] - stats: improve code quality

--- backend/src/infraestructure/SongsStats.py
+++ backend/src/infraestructure/SongsStats.py
@@ -3,26 +3,29 @@ import os.path
 
 from pydblite import Base
 
+
 class SongsStats:
-    def __init__(self):
-        self.file = './src/infraestructure/stats.pdl'
+    def __init__(self, filename=None):
+        basePath = "./src/infraestructure"
+        dbFile = filename or 'stats'
+        self.file = f'{basePath}/{dbFile}.pdl'
 
     def setUpDatabase(self) -> None:
-        if not os.path.exists(self.file) :
+        if not os.path.exists(self.file):
             db = Base(self.file)
             db.create('uri', 'offset', 'likes', 'dislikes')
             db.create_index('uri', 'offset', 'likes', 'dislikes')
 
-    def getDatabase():
-        SongsStats().setUpDatabase()
-        return Base(SongsStats().file)
+    def getDatabase(self) -> Base:
+        self.setUpDatabase()
+        return Base(self.file)
 
-    def addSong(albumUri: str, songOffset: int):
-        db = SongsStats.getDatabase()
+    def addSong(self, albumUri: str, songOffset: int) -> object:
+        db = self.getDatabase()
 
         db.open()
         record = db(uri=albumUri, offset=songOffset)
-        if not record :
+        if not record:
             db.insert(
                 albumUri,
                 songOffset,
@@ -36,14 +39,14 @@ class SongsStats:
             "id": record[0]['__id__']
         }
 
-    def likeSong(albumUri: str, songOffset: int):
-        db = SongsStats.getDatabase()
+    def likeSong(self, albumUri: str, songOffset: int):
+        db = self.getDatabase()
         db.open()
         exists = db(uri=albumUri, offset=songOffset)
         if exists:
             db.update(
                 exists[0],
-                likes=exists[0]['likes']+1
+                likes=exists[0]['likes'] + 1
             )
             db.commit()
             return {"success": True}
@@ -51,14 +54,14 @@ class SongsStats:
             "error": "song is not registered"
         }
 
-    def dislikeSong(albumUri: str, songOffset: int):
-        db = SongsStats.getDatabase()
+    def dislikeSong(self, albumUri: str, songOffset: int):
+        db = self.getDatabase()
         db.open()
         exists = db(uri=albumUri, offset=songOffset)
         if exists:
             db.update(
                 exists[0],
-                dislikes=exists[0]['dislikes']+1
+                dislikes=exists[0]['dislikes'] + 1
             )
             db.commit()
             return {"success": True}
@@ -66,8 +69,8 @@ class SongsStats:
             "error": "song is not registered"
         }
 
-    def songIsBanned(albumUri: str, songOffset: int) -> bool:
-        db = SongsStats.getDatabase()
+    def songIsBanned(self, albumUri: str, songOffset: int) -> bool:
+        db = self.getDatabase()
         db.open()
         exists = db(uri=albumUri, offset=songOffset)
         if exists:
@@ -80,13 +83,13 @@ class SongsStats:
                 return False
 
             hasMinimumValidations = totalVotes > 10
-            hasFewerLikes = (songDislikes/(totalVotes)) >= threshold
+            hasFewerLikes = (songDislikes / (totalVotes)) >= threshold
             return hasMinimumValidations and hasFewerLikes
 
         return False
 
-    def showSongs() -> None:
-        db = SongsStats.getDatabase()
+    def showSongs(self) -> None:
+        db = self.getDatabase()
 
         db.open()
         for register in db:
